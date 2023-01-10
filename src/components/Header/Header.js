7d7146b9079508fba789bd0e4691feb094e190b9
JoaoVitorLima242 <jlima0500@gmail.com>
2023-01-10 16:37:49 -0300

    using differents queries

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,5 +4,10 @@ import "./Header.css"
 export default function Header({
     title
 }) {
-    return <h1 className="header">{title}</h1>
+    return (
+        <>
+            <h1 className="header">Cats</h1>
+            <h1 className="header">{title}</h1>
+        </>
+    )
 }
--- src/components/Header/__test__/Header.test.js
+++ src/components/Header/__test__/Header.test.js
@@ -2,10 +2,39 @@ import { render, screen } from '@testing-library/react'
 
 import Header from '../Header'
 
+// GET BY
 it('Should render the same text passed into title prop', () => {
     render(<Header title='My Header'/>)
 
     const headingElement = screen.getByText(/my header/i)
 
     expect(headingElement).toBeInTheDocument()
+})
+
+it('Should render a heading in our component with a text passed by title prop', () => {
+    render(<Header title='My Header'/>)
+
+    const headingElement = screen.getByRole('heading', {
+        name: 'My Header'
+    })
+
+    expect(headingElement).toBeInTheDocument()
+})
+
+// FIND BY
+it('Should render the same text passed into title prop using find', async () => {
+    render(<Header title='My Header'/>)
+
+    const headingElement = await screen.findByText(/my header/i)
+
+    expect(headingElement).toBeInTheDocument()
+})
+
+// QUERY BY
+it('Should render the same text passed into title prop using query', async () => {
+    render(<Header title='My Header'/>)
+
+    const headingElement = screen.queryByText(/dogs/i)
+
+    expect(headingElement).not.toBeInTheDocument()
 })
