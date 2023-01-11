a1041ed86102dbe29a2205fb939a1b85ec7555cc
JoaoVitorLima242 <jlima0500@gmail.com>
2023-01-11 15:11:32 -0300

    remove useless console log

--- src/components/FollowersList/FollowersList.js
+++ src/components/FollowersList/FollowersList.js
@@ -13,7 +13,6 @@ export default function FollowersList() {
 
     const fetchFollowers = async () => {
         const {data} = await axios.get("https://randomuser.me/api/?results=5")
-        console.log(data)
         setFollowers(data.results)
     }
 
