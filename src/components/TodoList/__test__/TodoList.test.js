548d27f271bd92924104aa73e6f4caa2f6876a9c
JoaoVitorLima242 <jlima0500@gmail.com>
2023-01-11 12:00:24 -0300

    add tests to toDoList

---
+++ src/components/TodoList/__test__/TodoList.test.js
@@ -0,0 +1,43 @@
+import { fireEvent, render, screen } from "@testing-library/react"
+
+import TodoList from "../TodoList"
+import { BrowserRouter } from "react-router-dom"
+
+const mockedSetTodos = jest.fn()
+const MockTodoList = ({
+    todos
+}) => (
+    <BrowserRouter>
+        <TodoList 
+            todos={todos}
+            setTodos={mockedSetTodos}
+        />
+    </BrowserRouter>
+)
+
+describe('TodoList', () => {
+    it('Should initialize with 0 todos on the list', () => {
+        render(
+            <MockTodoList
+                todos={[]}
+            />
+        )
+        
+        const toDoElements = screen.queryAllByTestId('todoWrapper')
+
+        expect(toDoElements.length).toBe(0)
+
+    })
+    
+    it('Should render more than one todo', () => {
+        render(
+            <MockTodoList
+                todos={['one', 'two', 'three']}
+            />
+        )
+        const toDoElements = screen.getAllByTestId('todoWrapper')
+
+
+        expect(toDoElements.length).toBe(3)
+    })
+})
